0eb01fc2126a8230e7ea9d96c4aa0d8f84231451
Suraj Pai <surajballambat@gmail.com>
2017-07-01 16:36:22 +0530

    task: added well fitted GBM model

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import roc_auc_score, make_scorer
+import numpy as np
 try:
     train_X = pd.read_csv('/home/suraj/Repositories/IndiaHacks ML Hackathon/train_X.csv')
     train_Y = pd.read_csv('/home/suraj/Repositories/IndiaHacks ML Hackathon/train_Y.csv')
@@ -10,9 +12,16 @@ try:
 except:
     print "Read Error"
 
-model = RandomForestClassifier(n_estimators=500, n_jobs=-1, max_depth=5, verbose=True)
-model.fit(train_X, train_Y)
-results = model.predict_proba(test_X)
+train_Y = np.asarray(train_Y['Labels'])
+print train_Y.shape
+model = GradientBoostingClassifier(n_estimators=800, verbose=True)
+param_grid = {
+    'max_depth': [5, 8]
+}
+scorer = make_scorer(roc_auc_score)
+cv_model = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scorer, verbose=True, n_jobs=-1, cv=3)
+cv_model.fit(train_X, train_Y)
+results = cv_model.predict_proba(test_X)
 results = pd.DataFrame(columns=['seg', 'segment'], data=results)
 results.drop('seg', axis=1, inplace=True)
 results['ID'] = label
